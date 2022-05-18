98c6c61fa9a111a0457012dcda9dae488a548810
glennlum <glenn@horangi.com>
2022-05-18 14:21:55 +0800

    Add project 1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GraphQL-with-Flask-and-Python
 GraphQL projects with Flask and Python
+
+## Project 1
+This code includes a simple GraphQL application built with Flask, SQLAlchemy and Graphene ([source](https://docs.graphene-python.org/projects/sqlalchemy/en/latest/tutorial/))
---
+++ p1-flask-sqlalchemy-graphene/create_db.py
@@ -0,0 +1,19 @@
+from models import engine, db_session, Base, Department, Employee
+
+Base.metadata.create_all(bind=engine)
+
+# Create department records
+engineering = Department(name='Engineering')
+db_session.add(engineering)
+hr = Department(name='Human Resources')
+db_session.add(hr)
+
+# Create employee records
+peter = Employee(name='Peter', department=engineering)
+db_session.add(peter)
+roy = Employee(name='Roy', department=engineering)
+db_session.add(roy)
+tracy = Employee(name='Tracy', department=hr)
+db_session.add(tracy)
+
+db_session.commit()
